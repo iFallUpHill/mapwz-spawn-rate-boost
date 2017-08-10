b2c4e03db9ddf3cc13b05752fa44c13b73d38c91
Jason Manson-Hing <jason.mansonhing@gmail.com>
2017-08-09 22:26:24 -0400

    Added max monster limit (with shuffle) for exact numbers of mobs/map

--- README.md
+++ README.md
@@ -2,14 +2,17 @@
 
 For multiplying the spawn rate on a private server.
 
-Only accepts whole number multiplier rates.
+Only accepts whole number multiplier rates or monster limits.
 
 ```javascript
 // Install App
 npm install
 // Run App
-node spawnbuff.js --file="100010000.img.xml" --rate=2
-//  Outputs to a new XML with an improved spawnrate
-// i.e. 2x_100010000.img.xml 
+node spawnbuff.js --file="100010000.img.xml" --spawnRate=2
+//  Outputs to a new XML with an 2x mob limit
+//  >>> spawnRate_2x_100010000.img.xml 
+node spawnbuff.js --file="100010000.img.xml" --mobLimit=2
+//  Outputs to a new XML with an a mob limit of 105
+//  >>> maxSpawns_105_100010000.img.xml 
 ```
 
--- spawnbuff.js
+++ spawnbuff.js
@@ -4,40 +4,90 @@ var fs 		= require('fs'),
 
 var parser = new xml2js.Parser(),
     xmlBuilder = new xml2js.Builder(),
-    rateIncrease = argv.rate ? Math.round(argv.rate) : 1,
+    rateIncrease = argv.spawnRate ? Math.round(argv.spawnRate) : false,
+    mobLimit = argv.mobLimit ? Math.round(argv.mobLimit) : false,
     fileName = argv.file;
+    
+if (fileName) {
+	fs.readFile(fileName, function(err, data) {
+		if (err) {
+			console.log("Error reading file: " + err);
+		} else {
+			parser.parseString(data, function (err, result) {
+				if (err) {
+				console.log("Error parsing file: " + err);
+				} else {
+					var outerData = result.imgdir.imgdir;
+			        var lifePos = outerData.map(function(e) { return e.$.name; }).indexOf('life');
 
-fs.readFile(fileName, function(err, data) {
-    parser.parseString(data, function (err, result) {
+			        var existinglifeData = outerData[lifePos].imgdir;
 
-        var outerData = result.imgdir.imgdir;
-        var lifePos = outerData.map(function(e) { return e.$.name; }).indexOf('life');
+			        var mobOnlyData = existinglifeData.filter(function (obj) {
+			        	var stringPos = obj.string.map(function(e) { return e.$.name; }).indexOf('type');
+			        	return obj.string[stringPos].$.value == 'm';
+			        });
 
-        var existinglifeData = outerData[lifePos].imgdir;
+					var dataLength = mobOnlyData.length;
+					var existingLength = existinglifeData.length;
+					var modifiedPrefix = ''
+					if (mobLimit && mobLimit > dataLength) {
+						var shuffledMobData = shuffle(mobOnlyData.slice());
+						for(var i = 0; i < mobLimit - dataLength; i++) {
+							if(shuffledMobData.length == 0) {
+								shuffledMobData = shuffle(mobOnlyData.slice());
+								console.log(mobOnlyData)
+							}
 
-        var mobOnlyData = existinglifeData.filter(function (obj) {
-        	var stringPos = obj.string.map(function(e) { return e.$.name; }).indexOf('type');
-        	return obj.string[stringPos].$.value == 'm';
-        });
+							// Get Random Mob on Map
+				         	var dupeMob = JSON.parse(JSON.stringify(shuffledMobData.shift()));
+				         	dupeMob.$.name = (existingLength++).toString();
+							existinglifeData.push(dupeMob);
+						}
+				        modifiedPrefix = 'maxSpawns_' + mobLimit + '_';
+					} else if (rateIncrease && rateIncrease > 1) {
+						for(var i = 1; i < rateIncrease; i++) {
+				        	for(var j = 0; j < dataLength; j++) {
+				         		var dupeMob = JSON.parse(JSON.stringify(mobOnlyData[j]));
 
-		var dataLength = mobOnlyData.length;
-		var existingLength = existinglifeData.length;
+				         		dupeMob.$.name = (existingLength++).toString();
+								existinglifeData.push(dupeMob);
+				         	}
+				        }
+				        modifiedPrefix = 'spawnRate_' + rateIncrease + 'x_';
+					} else {
+						console.log("No valid rate increase or spawn limit specified!")
+					}
 
+			        result.imgdir.imgdir[lifePos].imgdir = existinglifeData;
 
-        for(var i = 1; i < rateIncrease; i++) {
-        	for(var j = 0; j < dataLength; j++) {
-         		var dupeMob = JSON.parse(JSON.stringify(mobOnlyData[j]));
+			        var xml = xmlBuilder.buildObject(result);
 
-         		dupeMob.$.name = (parseInt(dupeMob.$.name) + existingLength).toString();
-				existinglifeData.push(dupeMob);
-         	}
-         	existingLength = existinglifeData.length;
-        }
-        result.imgdir.imgdir[lifePos].imgdir = existinglifeData;
+			        fs.writeFile(modifiedPrefix+ fileName, xml, function(err) {});
+				}
+		    });
+		}
+	});
+} else {
+	console.log("No file specified");
+}
 
-        var xml = xmlBuilder.buildObject(result);
+// From https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+function shuffle(array) {
+    let counter = array.length;
 
-        fs.writeFile(rateIncrease + 'x_' + fileName, xml, function(err) {});
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
 
-    });
-});
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
